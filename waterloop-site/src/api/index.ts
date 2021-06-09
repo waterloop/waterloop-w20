6b75daf85dea91a029ffde05be316293ff0c21f3
Aayush Wadhwa <aayushwadhwa31@gmail.com>
2021-06-09 20:13:18 +0530

    Hooks Integrated

--- waterloop-site/src/api/index.ts
+++ waterloop-site/src/api/index.ts
@@ -2,9 +2,11 @@ import postings from './postings';
 import server from './server';
 import sponsors from './sponsors';
 import teams from './teams';
+import blogs from './blogs';
 
 export default {
   postings: postings(server),
   teams: teams(server),
   sponsors: sponsors(server),
+  blogs:  blogs(server)
 };
